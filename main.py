c71ccc7b224c3729a15f1568a6454440459c8ca6
kaser137 <107580630+kaser137@users.noreply.github.com>
2023-04-28 08:44:27 +0300

    add images to the books

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
 from pathlib import Path
+from urllib.parse import urljoin, urlsplit, unquote
 
 
 def check_for_redirect(r):
@@ -10,30 +11,48 @@ def check_for_redirect(r):
         raise requests.HTTPError
 
 
+def get_response(url, params={}):
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    check_for_redirect(response)
+    return response
+
+
 def parsing_book(book_id):
     url = f'https://tululu.org/b{book_id}'
     r = requests.get(url)
-    r.raise_for_status()
     soup = BeautifulSoup(r.text, 'lxml')
     title, author = soup.find('h1').text.split('::')
-    return {'title': title.strip(), 'author': author.strip()}
+    img = urljoin('https://tululu.org/', soup.find(class_='bookimage').find('img')['src'])
+    return {'title': title.strip(), 'author': author.strip(), 'img': img}
 
 
-def download_txt(book_id, folder='books/'):
+def download_txt(book_id, name, folder='books/'):
     Path(folder).mkdir(parents=True, exist_ok=True)
     url = f'https://tululu.org/txt.php?id={book_id}'
-    r = requests.get(url)
-    r.raise_for_status()
+    r = get_response(url)
+    filename = f'{sanitize_filename(name)}.txt'
+    filepath = os.path.join(folder, filename)
+    with open(filepath, 'w') as file:
+        file.write(r.text)
+    return filepath
+
+
+def download_img(url, folder='images/'):
+    Path(Path.cwd() / folder).mkdir(parents=True, exist_ok=True)
+    filename = unquote(urlsplit(url).path).split('/')[-1]
+    r = get_response(url)
+    filepath = os.path.join(folder, sanitize_filename(filename))
+    with open(filepath, 'wb') as file:
+        file.write(r.content)
+    return filepath
+
+
+
+for i in range(1,11):
     try:
-        check_for_redirect(r)
-        filename = ''.join((sanitize_filename(parsing_book(book_id)['title']), '.txt'))
-        filepath = os.path.join(folder, filename)
-        with open(filepath, 'w') as file:
-            file.write(r.text)
-        return filepath
+        url = f'https://tululu.org/txt.php?id={i}'
+        r = get_response(url)
+        download_img(parsing_book(i)['img'])
     except requests.HTTPError:
-        return f"book with id: {book_id} hasn't downloaded"
-
-
-for i in range(1, 11):
-    print(f'book {i}   ', download_txt(i))
+        continue
