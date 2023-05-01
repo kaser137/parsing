c3403353e3d96ec177cfdad06db768874a29d55f
kaser137 <107580630+kaser137@users.noreply.github.com>
2023-05-01 07:00:07 +0300

    add args in cmd

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -9,34 +9,43 @@ from urllib.parse import urljoin, urlsplit, unquote
 from time import sleep
 import main
 
+def main1():
+    parser = argparse.ArgumentParser(description='Getting books')
+    parser.add_argument('--start_page', type=int, default=1,
+                        help='page for start, must be >= 1')
+    parser.add_argument('--end_page', type=int, default=701,
+                        help='page for end, must be >= start_id')
+    args = parser.parse_args()
+    for page in range(args.start_page, args.end_page+1):
+        url =f'https://tululu.org/l55/{page}/'
+        response = requests.get(url)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, 'lxml')
+        books_on_page = soup.select('.d_book ')
+        for book in books_on_page:
+            # bookpath = urljoin(response.url, book.select_one('a')['href'])
+            book_id = book.select_one('a')['href'][2:-1]
+            url = f'https://tululu.org/b{book_id}/'
+            try:
+                book_details = main.parse_book_page(main.get_response(url))
+                main.download_txt(book_id, book_details['title'])
+                main.download_img(book_details['img'])
+                book_info = {
+                    'title': book_details['title'],
+                    'author': book_details['author'],
+                    'imc_src': f'images/{unquote(urlsplit(book_details["img"]).path).split("/")[-1]}',
+                    'book_path': f'books/{sanitize_filename(book_details["title"])}.txt',
+                    'comments': book_details['comments'],
+                    'genres': book_details['genres'],
+                }
+                books_json = json.dumps(book_info, indent=0, ensure_ascii=False, separators=(', ', ': '))
+                with open('books.json', 'a', encoding='utf8') as file:
+                    file.write(books_json+',\n')
+            except requests.HTTPError:
+                print(f'book with number {book_id} is absent')
 
-books_info = []
-for i in range(1, 2):
-    url =f'https://tululu.org/l55/{i}/'
-    response = requests.get(url)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.text, 'lxml')
-    books_on_page = soup.select('.d_book ')
-    for book in books_on_page:
-        bookpath = urljoin(response.url, book.select_one('a')['href'])
-        book_id = book.select_one('a')['href'][2:-1]
-        url = f'https://tululu.org/b{book_id}/'
-        try:
-            book_details = main.parse_book_page(main.get_response(url))
-            main.download_txt(book_id, book_details['title'])
-            main.download_img(book_details['img'])
-            book_info = {
-                'title': book_details['title'],
-                'author': book_details['author'],
-                'imc_src': f'images/{unquote(urlsplit(book_details["img"]).path).split("/")[-1]}',
-                'book_path': f'books/{sanitize_filename(book_details["title"])}.txt',
-                'comments': book_details['comments'],
-                'genres': book_details['genres'],
-            }
-            books_json = json.dumps(book_info, indent=0, ensure_ascii=False, separators=(', ', ': '))
-            with open('books.json', 'a', encoding='utf8') as file:
-                file.write(books_json+',\n')
-        except requests.HTTPError:
-            print(f'book with number {book_id} is absent')
+
+if __name__ == '__main__':
+    main1()
 
 
