9fdd0aa6d5fa567eba34bd38daf064f700ceb4d9
kaser137 <107580630+kaser137@users.noreply.github.com>
2023-05-06 00:37:44 +0300

    Add links to books txt files.

--- httml_books.py
+++ httml_books.py
@@ -1,6 +1,6 @@
 import json
 from http.server import HTTPServer, SimpleHTTPRequestHandler
-
+from pathlib import Path
 from urllib.parse import quote
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
@@ -12,26 +12,31 @@ from more_itertools import chunked
 with open('media/books.json', 'r', encoding='utf-8') as file:
     books_json = file.read()
 books = json.loads(books_json)
-books_col = list(chunked(books, 2))
+# books_col = list(chunked(books, 2))
 for book in books:
-    book['book_path'] = quote(book['book_path'])
-
+    book['book_path'] = Path(Path.cwd(), quote(book['book_path'])).as_posix()
 
 
 def on_reload():
-    env = Environment(
-        loader=FileSystemLoader('.'),
-        autoescape=select_autoescape(['html', 'xml']))
-    template = env.get_template('template.html')
-    rendered_page = template.render(
-        books_col=books_col,
+    Path(Path.cwd() / 'pages').mkdir(parents=True, exist_ok=True)
+    books_pages = list(chunked(books, 20))
+    for num_page, books_on_page in enumerate(books_pages):
+        books_col = list(chunked(books_on_page, 2))
+        env = Environment(
+            loader=FileSystemLoader('.'),
+            autoescape=select_autoescape(['html', 'xml']))
+        template = env.get_template('template.html')
+        rendered_page = template.render(
+            books_col=books_col,
+            prev=Path(Path.cwd(), 'pages', f'index{num_page-1}.html').as_posix() if num_page else Path(Path.cwd(), 'pages', f'index{num_page}.html').as_posix(),
+            next=Path(Path.cwd(), 'pages', f'index{num_page+1}.html').as_posix()
 
-    )
-    with open('index.html', 'w', encoding="utf-8") as file:
-        file.write(rendered_page)
+        )
+        with open(Path('pages', f'index{num_page}.html'), 'w', encoding="utf-8") as file:
+            file.write(rendered_page)
 
 
 on_reload()
 server = Server()
 server.watch('template.html', on_reload)
-server.serve(root='.')
+server.serve(root='pages/index0.html')
