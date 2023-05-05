31d508ba2326d84236883f1b826612078711fe0e
kaser137 <107580630+kaser137@users.noreply.github.com>
2023-05-05 18:18:56 +0300

    Add links to books txt files.

--- httml_books.py
+++ httml_books.py
@@ -1,19 +1,21 @@
 import json
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+from urllib.parse import quote
+
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 from livereload import Server, shell
 
 from more_itertools import chunked
 
-
-
-
 with open('media/books.json', 'r', encoding='utf-8') as file:
     books_json = file.read()
 books = json.loads(books_json)
-books_col = list(chunked(books,2))
+books_col = list(chunked(books, 2))
+for book in books:
+    book['book_path'] = quote(book['book_path'])
+
 
 
 def on_reload():
@@ -22,7 +24,8 @@ def on_reload():
         autoescape=select_autoescape(['html', 'xml']))
     template = env.get_template('template.html')
     rendered_page = template.render(
-        books_col=books_col
+        books_col=books_col,
+
     )
     with open('index.html', 'w', encoding="utf-8") as file:
         file.write(rendered_page)
