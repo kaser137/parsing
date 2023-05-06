4e73f9324df380bf10aac0eadf85b7f09f4cfa6e
kaser137 <107580630+kaser137@users.noreply.github.com>
2023-05-06 10:39:00 +0300

    Add paginator.

--- httml_books.py
+++ httml_books.py
@@ -12,26 +12,38 @@ from more_itertools import chunked
 with open('media/books.json', 'r', encoding='utf-8') as file:
     books_json = file.read()
 books = json.loads(books_json)
-# books_col = list(chunked(books, 2))
+
 for book in books:
-    book['book_path'] = Path(Path.cwd(), quote(book['book_path'])).as_posix()
+    book['book_path'] = f'../{quote(book["book_path"])}'
 
 
 def on_reload():
     Path(Path.cwd() / 'pages').mkdir(parents=True, exist_ok=True)
     books_pages = list(chunked(books, 20))
-    for num_page, books_on_page in enumerate(books_pages):
-        books_col = list(chunked(books_on_page, 2))
+
+    for num_page, books_on_page in enumerate(books_pages, 1):
+        books_split_for_columns = list(chunked(books_on_page, 2))
+        pages_quantity = len(books_pages)
         env = Environment(
             loader=FileSystemLoader('.'),
             autoescape=select_autoescape(['html', 'xml']))
         template = env.get_template('template.html')
+        if num_page == 1:
+            prev = num_page
+            next = num_page + 1
+        elif num_page == pages_quantity:
+            prev = num_page - 1
+            next = num_page
+        else:
+            prev = num_page - 1
+            next = num_page + 1
         rendered_page = template.render(
-            books_col=books_col,
-            prev=Path(Path.cwd(), 'pages', f'index{num_page-1}.html').as_posix() if num_page
-            else Path(Path.cwd(), 'pages', f'index{num_page}.html').as_posix(),
-            next=Path(Path.cwd(), 'pages', f'index{num_page+1}.html').as_posix()
-
+            books_split_for_columns=books_split_for_columns,
+            pages_quantity=pages_quantity,
+            prev=f'../pages/index{prev}.html',
+            next=f'../pages/index{next}.html',
+            current=lambda x: f'../pages/index{x}.html',
+            num_page=num_page
         )
         with open(Path('pages', f'index{num_page}.html'), 'w', encoding="utf-8") as file:
             file.write(rendered_page)
@@ -40,4 +52,4 @@ def on_reload():
 on_reload()
 server = Server()
 server.watch('template.html', on_reload)
-server.serve(root='pages/index0.html')
+server.serve(default_filename='pages/index1.html')
